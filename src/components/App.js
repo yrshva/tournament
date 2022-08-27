df8da8aee713601908ab1d21eeab415aa44f4b49
jesjonesc <58461507+yrshva@users.noreply.github.com>
2022-08-28 01:56:25 +0300

    added score table, displayed teams

--- src/components/App.js
+++ src/components/App.js
@@ -1,40 +1,50 @@
 import { useState } from "react";
 import Teams from "./Teams";
+import { uid } from "uid";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../styles/App.css";
 
 function App() {
-  const [input, setInput] = useState("");
-  const [team, setTeam] = useState(null);
-  function updateInput(event) {
-    setInput(event.target.value);
-  }
+  const [teams, setTeams] = useState([]);
+  const [loaded, setLoaded] = useState(false);
+
   function handleSubmit(event) {
     event.preventDefault();
-    setTeam(input);
-    setInput("");
+    console.log(teams);
+    setTeams([
+      ...teams,
+      {
+        id: uid(),
+        name: event.target.team_name.value,
+        played: 0,
+        win: 0,
+        draw: 0,
+        lost: 0,
+      },
+    ]);
+    event.target.team_name.value = "";
+    setLoaded(true);
   }
   function showSearch() {
     return (
       <form className="d-flex justify-content-center" onSubmit={handleSubmit}>
         <input
           type="text"
+          name="team_name"
           placeholder="New team"
           className="form-control me-3 input-width"
-          onChange={updateInput}
-          value={input}
         />
         <input type="submit" placeholder="Add" className="btn btn-dark" />
       </form>
     );
   }
 
-  if (team) {
+  if (loaded) {
     return (
       <div className="app-container">
         <div className="container verital-align">
           {showSearch()}
-          <Teams name={team} />
+          <Teams teamsData={teams} />
         </div>
       </div>
     );
--- src/components/Teams.js
+++ src/components/Teams.js
@@ -1,30 +1,87 @@
+import { uid } from "uid";
+
 export default function Teams(props) {
-  const name = props.name;
-  console.log(name);
+  const teams = props.teamsData;
+  let matches = [];
+  function permutation(array, length) {
+    return (matches = array.flatMap((element, i) =>
+      length > 1
+        ? permutation(array.slice(i + 1), length - 1).map((el) => [
+            element,
+            ...el,
+          ])
+        : [[element]]
+    ));
+  }
+  permutation(teams, 2);
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    teams.map((team, index) => console.log(event.target.team.id.value));
+  }
+
   return (
-    <table className="table table-hover table-dark mt-4">
-      <thead>
-        <tr>
-          <th scope="col">Place</th>
-          <th scope="col">Team</th>
-          <th scope="col">Played</th>
-          <th scope="col">Win</th>
-          <th scope="col">Draw</th>
-          <th scope="col">Lost</th>
-          <th scope="col">Points</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <th scope="row">1</th>
-          <td>{name}</td>
-          <td>0</td>
-          <td>0</td>
-          <td>0</td>
-          <td>0</td>
-          <td>0</td>
-        </tr>
-      </tbody>
-    </table>
+    <div className="tables row">
+      <div className="col-12 col-md-8 score-table d-flex justify-content-center">
+        <table className="table table-dark table-bordered table-width">
+          <thead className="thead-dark">
+            <tr>
+              <th scope="col">Place</th>
+              <th scope="col">Team</th>
+              <th scope="col">Played</th>
+              <th scope="col">Win</th>
+              <th scope="col">Draw</th>
+              <th scope="col">Lost</th>
+              <th scope="col">Points</th>
+            </tr>
+          </thead>
+          <tbody>
+            {teams.length > 0 ? (
+              teams.map((team, index) => (
+                <tr key={index}>
+                  <th scope="row">{index + 1}</th>
+                  <td>{team.name}</td>
+                  <td>{team.played}</td>
+                  <td>{team.win}</td>
+                  <td>{team.draw}</td>
+                  <td>{team.lost}</td>
+                  <td>{team.win * 3 + team.draw}</td>
+                </tr>
+              ))
+            ) : (
+              <></>
+            )}
+          </tbody>
+        </table>
+      </div>
+      <div className="games-list col-12 col-md-4  d-flex justify-content-center">
+        <table>
+          <tbody>
+            {teams.length > 1 &&
+              matches.map((match, index) => (
+                <tr key={index}>
+                  {match.map((col, index) =>
+                    index % 2 === 0 ? (
+                      <td key={index}>
+                        {col.name}
+                        <form onSubmit={handleSubmit}>
+                          <input type="text" name={col.id} />
+                        </form>
+                      </td>
+                    ) : (
+                      <td key={index}>
+                        <form onSubmit={handleSubmit}>
+                          <input type="text" name={col.id} />
+                        </form>
+                        {col.name}
+                      </td>
+                    )
+                  )}
+                </tr>
+              ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
   );
 }
